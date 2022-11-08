0f5719f3b239d07ddaff67d86c578217f4c452c0
felipe <4548462626@estudante.sed.sc.gov.br>
2022-11-08 14:41:13 -0200

    primeiro

---
+++ sqlBanco/banco.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Cliente(
+	id_cliente int primary key auto_increment,
+	nome varchar(255),
+	senha varchar(50)
+	);
+
+INSERT INTO cliente (nome, senha) VALUES ('Brenon', '1234'), (admin, '1234');
